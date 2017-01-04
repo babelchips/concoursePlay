0223ce1b3fd9d41f4fa9b6ed21239cd88bb816ac
Chris Dewar-English <chris.dewar-english@bond.co.uk>
2017-01-04 15:34:41 +0000

    Try getting params from different sources

--- task/generate-output.sh
+++ task/generate-output.sh
@@ -6,6 +6,8 @@ if [ ! -d generated-output-folder ]; then
 	mkdir generated-output-folder
 fi
 
-echo $MYPARAM1 > generated-output-folder/output.txt
+echo $MYPARAM1 >> generated-output-folder/output.txt
+echo $MYPARAM2 >> generated-output-folder/output.txt
+echo $MYPARAM3 >> generated-output-folder/output.txt
 
 echo FINISHED.
